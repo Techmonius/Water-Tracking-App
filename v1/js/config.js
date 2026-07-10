b1f7330bad14b9b215d9df6c064c698fabd8087f
Techmonius <soprondek@gmail.com>
2026-07-10 09:47:38 -0500

    Bump cohesive build to beta6

--- v1/js/config.js
+++ v1/js/config.js
@@ -1,5 +1,5 @@
 window.WT_V1_CONFIG = Object.freeze({
-  appVersion: '1.0.0-beta.5',
+  appVersion: '1.0.0-beta.6',
   schemaVersion: 2,
   storageKey: 'waterTracker_v2',
   legacyKeys: Object.freeze({
