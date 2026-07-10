8accc92d32481de3e6804a50419a15b9a2acee19
Techmonius <soprondek@gmail.com>
2026-07-09 22:32:12 -0500

    Add v1 configuration module

---
+++ v1/js/config.js
@@ -0,0 +1,14 @@
+window.WT_V1_CONFIG = Object.freeze({
+  appVersion: '1.0.0-beta.1',
+  schemaVersion: 2,
+  storageKey: 'waterTracker_v2',
+  legacyKeys: Object.freeze({
+    hydration: 'waterTracker_v1',
+    achievements: 'waterTracker_engagement_v1',
+    dailyWins: 'waterTracker_dailyWins_v1',
+    celebrations: 'waterTracker_goalCelebrated_v1'
+  }),
+  telemetryInstallKey: 'waterTracker_installId',
+  defaultGoal: 120,
+  defaultWeekendGoal: 100
+});
