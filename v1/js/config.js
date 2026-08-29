1c960b47b320704598baf965898f88676d11886e
Techmonius <soprondek@gmail.com>
2026-08-29 11:57:47 -0500

    Bump version to 1.6.4

--- v1/js/config.js
+++ v1/js/config.js
@@ -1,5 +1,5 @@
 window.WT_V1_CONFIG = Object.freeze({
-  appVersion: '1.6.3',
+  appVersion: '1.6.4',
   schemaVersion: 2,
   storageKey: 'waterTracker_v2',
   legacyKeys: Object.freeze({hydration:'waterTracker_v1',achievements:'waterTracker_engagement_v1',dailyWins:'waterTracker_dailyWins_v1',celebrations:'waterTracker_goalCelebrated_v1'}),
